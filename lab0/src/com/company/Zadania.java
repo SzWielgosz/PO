830c7f810e37343a5ff3f844c441f301ace8cf0c
SzWielgosz <77066615+SzWielgosz@users.noreply.github.com>
2022-03-28 18:47:23 +0200

    lab0

---
+++ lab0/src/com/company/Zadania.java
@@ -0,0 +1,79 @@
+package com.company;
+
+public class Zadania {
+    public static void main(String[] args) {
+        //Zad1
+        System.out.println(31 + 29 + 31);
+        //Zad2
+        System.out.println(1 + 2 + 3 + 4 + 5 + 6 + 7 + 8 + 9 + 10);
+        //Zad3
+        System.out.println(2 * 3 * 4 * 5 * 6 * 7 * 8 * 9 * 10);
+        //Zad4
+        float saldo = 1000;
+        saldo -= (saldo * (6.0 / 100));
+        saldo -= (saldo * (6.0 / 100));
+        saldo -= (saldo * (6.0 / 100));
+        System.out.println(saldo);
+        //Zad5
+        System.out.println("|------|");
+        System.out.println("|Janusz|");
+        System.out.println("|------|");
+        //Zad6
+        System.out.println("  /////");
+        System.out.println(" +''''+");
+        System.out.println("(| o o |)");
+        System.out.println("  | ^ |");
+        System.out.println(" | ‘-’ |");
+        System.out.println(" +-----+");
+        //Zad7
+        System.out.println("****** ****** *   * * *   * *  ***  *    *      *     * * ****** *       *****  ***  ****** ******");
+        System.out.println("*          *   * *  *  * *  * *   * * *  *      *     * * *      *      *      *   * *          *");
+        System.out.println("******    *     *   *   *   * *   * *  * *      *     * * ****** *      *  *** *   * ******    *");
+        System.out.println("     *   *      *   *       * *   * *   **      *  *  * * *      *      *    * *   *      *   *");
+        System.out.println("****** ******   *   *       *  ***  *    *      *     * * ****** ****** ******  ***  ****** ******");
+        //Zad8
+        System.out.println("   +");
+        System.out.println("  + +");
+        System.out.println(" +   +");
+        System.out.println("+-----+");
+        System.out.println("| .-. |");
+        System.out.println("| | | |");
+        System.out.println("+_+_+_+");
+        //Zad9
+        System.out.println("     |----------|");
+        System.out.println("     | Greetings|");
+        System.out.println("     |----------|");
+        System.out.println("          ||    ");
+        System.out.println(" (\\__/)   ||");
+        System.out.println(" (•ㅅ•)   ||");
+        System.out.println("/      づ ");
+        //Zad10
+        System.out.println("Franek");
+        System.out.println("Marek");
+        System.out.println("Jarek");
+        //Zad11
+        System.out.println("Wisława Szymborska 'Dłoń'");
+        System.out.println("Dwadzieścia siedem kości,");
+        System.out.println("trzydzieści pięć mięśni,");
+        System.out.println("około dwóch tysięcy komórek nerwowych");
+        System.out.println("w każdej opuszce naszych pięciu palców.");
+        System.out.println("To zupełnie wystarczy,");
+        System.out.println("żeby napisać „Mein Kampf”");
+        System.out.println("albo „Chatkę Puchatka”. ");
+        //Zad12
+        System.out.println("* * * * * *==========================");
+        System.out.println(" * * * * * ==========================");
+        System.out.println("* * * * * *==========================");
+        System.out.println(" * * * * * ==========================");
+        System.out.println("* * * * * *==========================");
+        System.out.println(" * * * * * ==========================");
+        System.out.println("* * * * * *==========================");
+        System.out.println("=====================================");
+        System.out.println("=====================================");
+        System.out.println("=====================================");
+        System.out.println("=====================================");
+        System.out.println("=====================================");
+        System.out.println("=====================================");
+
+    }
+}
